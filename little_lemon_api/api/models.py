84e0177affb09496b6972c65efdfaa5f010bcf77
ksio2124 <ksio2124@protonmail.ch>
2024-09-13 15:53:55 -0400

    adding __str__ to Category and MenuItem

--- little_lemon_api/api/models.py
+++ little_lemon_api/api/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Category(models.Model):
+    def __str__(self) -> str:
+        return self.title
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True)
 
+
 class MenuItem(models.Model):
+    def __str__(self) -> str:
+        return self.title
     title = models.CharField(max_length=255, db_index=True, unique=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
@@ -23,12 +28,15 @@ class Cart(models.Model):
     class Meta:
         unique_together = ('menu_item', 'user')
 
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
+    delivery_crew = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
     status = models.BooleanField(db_index=True, default=0)
     total = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(db_index=True, auto_now=True)
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
